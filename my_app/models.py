1a40acb2a0118afab95443ba3433d36b362aceaa
peter fieldhouse <name>
2019-11-18 21:49:42 +0000

    added search

--- my_app/models.py
+++ my_app/models.py
@@ -4,5 +4,10 @@ class Search(models.Model):
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now=True)
 
+    #Admin will conver Search to Searchs so need to modify the 
+    #the Meta to correct the correct spelling
+    class Meta:
+        verbose_name_plural = 'Searches'
+
     def __str__(self):
-        return self.search
+        return f'{self.search}'
