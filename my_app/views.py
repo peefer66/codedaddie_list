311191d5286d9432e883febbf61ebacc022f3c4f
peter fieldhouse <name>
2019-11-22 21:29:17 +0000

    new_search.html

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+
 
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {'search':search}
+    
+    return render(request, 'my_app/new_search.html', context)
